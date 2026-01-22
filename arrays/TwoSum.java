f07f93827a9e21576c7df55da1eb4e132eafb5a8
Rohitha <rohithabalusu176@gmail.com>
2026-01-22 10:05:33 +0530

    Add Two Sum solution using HashMap

---
+++ arrays/TwoSum.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class TwoSum {
+
+    // Problem: Two Sum
+    // Given an array of integers nums and an integer target,
+    // return indices of the two numbers such that they add up to target.
+    //
+    // Approach: HashMap
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{}; // problem guarantees a solution
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + ", " + result[1]);
+    }
+}
