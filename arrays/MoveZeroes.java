e383be659fce8f102510a716ae53bb2868b3e59c
Rohitha <rohithabalusu176@gmail.com>
2026-01-23 10:35:22 +0530

    Add Move Zeroes Problem using optimal two-pointer approach

--- arrays/MoveZeroes.java
+++ arrays/MoveZeroes.java
@@ -5,7 +5,7 @@ class MoveZeroes {
         // Move all non-zero elements forward
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[index++] = nums[i];
+                nums[index++] = nums[i]; 
             }
         }
 
